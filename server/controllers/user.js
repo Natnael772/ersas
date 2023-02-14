593f0a774f03f8cd1a4a38c775591596bf01d69b
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-02-14 23:41:12 +0300

    Defined blog schema

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
+const User = require("../models/user");
 
 exports.getBlogs = (req, res, next) => {};
 
-exports.getBlog() = (req,res,next)=>{
-    
-}
+exports.getBlog = (req, res, next) => {};
 
 exports.createBlog = (req, res, next) => {};
 
---
+++ server/models/blog.js
@@ -0,0 +1,24 @@
+const app = require("express")();
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+  category: String,
+  subcategory: String,
+  title: String,
+  description: String,
+  claps: {
+    number: Number,
+    clappers: {
+      name: String,
+    },
+    comments: {
+      number: Number,
+      commenters: {
+        name: String,
+      },
+    },
+  },
+});
+
+module.exports = mongoose.model("Blog", blogSchema)
