a47143ab6fd3254e0ce6d40ae01e59d31f1fb071
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-02-14 15:30:59 +0300

    Configured mongodb

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
 const app = require("express")();
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://natnael:root@cluster0.gy0a5.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then((res) => {
+    app.listen(8080);
+    console.log("Server running on port 8080");
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
