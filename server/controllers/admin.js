2b336c36e2272bd3c0382dc603402630d3b7c8ce
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-02-15 01:20:36 +0300

    Added get user functionality for admin

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -8,5 +8,14 @@ exports.getUsers = async (req, res, next) => {
     users: users,
   });
 };
-exports.getUser = (req, res, next) => {};
+exports.getUser = async (req, res, next) => {
+  const userId = req.params.userId;
+  const user = await User.findById(userId);
+
+  //   const user = await User.find({ _id: userId });
+  res.json({
+    status: "success",
+    user: user,
+  });
+};
 exports.deleteUsers = (req, res, next) => {};
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,4 +4,5 @@ const router = require("express").Router();
 const adminController = require("../controllers/admin");
 
 router.get("/users", adminController.getUsers);
+router.get("/users/:userId", adminController.getUser);
 module.exports = router;
