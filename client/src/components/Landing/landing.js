9ecb7a501cc7f121a57ce7431db013de748a5f6e
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-03-13 23:59:45 +0300

    Landing page component

---
+++ client/src/components/Landing/landing.js
@@ -0,0 +1,15 @@
+const Landing = () => {
+  return (
+    <div className="hero-main-container">
+      <h1 className="hero-title-sub">Welcome to</h1>
+      <h1 className="hero-title-main">Ersas blog</h1>
+      <p className="hero-description">
+        Far far away, behind the word mountains, far from the countries Vokalia
+        and Consonantia, there live the blind texts. Separated they live in
+        Bookmarksgrove right at the coast of the Semantics, a large language
+        ocean.
+      </p>
+    </div>
+  );
+};
+export default Landing;
