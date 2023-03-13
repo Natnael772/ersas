906dc4410c32ce77f7a568ecc6811fe0fed9ccbe
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-03-13 21:14:47 +0300

    Set up Components

---
+++ client/src/Nav.js
@@ -0,0 +1,74 @@
+const Nav = () => {
+  return (
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="flex items-center justify-between w-full md:w-auto">
+            <a href="#" className="text-white text-2xl font-bold">
+              Logo
+            </a>
+            <div className="flex md:hidden">
+              <button
+                type="button"
+                className="text-gray-400 hover:text-white focus:outline-none focus:text-white"
+                aria-label="Toggle menu"
+              >
+                <svg viewBox="0 0 24 24" class="h-6 w-6 fill-current">
+                  <path
+                    fill-rule="evenodd"
+                    d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div className="hidden md:flex md:items-center md:justify-end md:flex-1">
+            <div className="flex flex-col md:flex-row md:space-x-4">
+              <a
+                href="#"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Home
+              </a>
+              <a
+                href="#"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                About
+              </a>
+              <a
+                href="#"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Contact
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hidden md:hidden">
+        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+          <a
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            About
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Nav;
---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,35 @@
+const Nav = () => {
+  return (
+    <div className="nav-main-container">
+      <h2 className="nav-logo">Ersas</h2>
+      <ul className="nav-menus-container">
+        <li>
+          {" "}
+          <a href="#" className="nav-links">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#" className="nav-links">
+            Blogs
+          </a>
+        </li>
+        <li>
+          <a href="#" className="nav-links">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#" className="nav-links">
+            Contact
+          </a>
+        </li>
+      </ul>
+      <ul className="nav-buttons-container">
+        <button className="btn-login">Log In</button>
+        <button className="btn-signup">Get started</button>
+      </ul>
+    </div>
+  );
+};
+export default Nav;
