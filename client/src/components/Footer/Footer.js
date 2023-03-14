4286a3cdd888399adc6e66b16d028820a9d48607
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-03-14 16:03:21 +0300

    Working on footer

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <div className="footer-main-container">
+      <div className="footer-intro">
+        <h2 className="footer-logo">Ersas</h2>
+        <p className="desc">
+          Far far away, behind the word mountains, far from the countries
+          Vokalia and Consonantia, there live the blind texts.
+        </p>
+      </div>
+      <div className="footer-latest">
+        <h2 className="footer-title">Latest Blog</h2>
+      </div>
+      <div className="footer-information">
+        <h2 className="footer-title">Information</h2>
+      </div>
+      <div className="footer-questions">
+        <h2 className="footer-title">Have any questions?</h2>
+      </div>
+    </div>
+  );
+};
