8a7dc528621c2c8d807963a1e95cd21ec578addf
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-03-18 10:40:07 +0300

    Working on footer component

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -26,12 +26,14 @@ const Footer = () => {
         </p>
       </div>
       <div className="footer-latest">
-        <h2 className="footer-title">Latest Blog</h2>
+        <h2 className="footer-title">latest News</h2>
         <div className="cards-container">
           <img className="footer-img" src={wp9} />
           <div className="desc-container">
             <h2 className="footer-blog-title">
-              Even all the powerful pointing has no control about
+              <a href="#" className="footer-blog-link">
+                Even all the powerful pointing has no control about
+              </a>
             </h2>
             <h3 className="footer-blog-date">Oct 16, 2019 . Admin</h3>
           </div>
@@ -40,7 +42,7 @@ const Footer = () => {
           <img className="footer-img" src={wp9} />
           <div className="desc-container">
             <h2 className="footer-blog-title">
-              <a href="footer-blog-link">
+              <a href="#" className="footer-blog-link">
                 Even all the powerful pointing has no control about
               </a>
             </h2>
@@ -50,9 +52,44 @@ const Footer = () => {
       </div>
       <div className="footer-information">
         <h2 className="footer-title">Information</h2>
+        <div className="infos-container">
+          <ul className="info-links">
+            <li>
+              <a href="#" className="info-link">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="info-link">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="info-link">
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#" className="info-link">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="footer-questions">
         <h2 className="footer-title">Have any questions?</h2>
+        <div className="contact-infos">
+          <div className="contact-info-con">
+            <h2 className="contact-info">Bole, Addis Ababa, Ethiopia</h2>
+          </div>
+          <div className="contact-info-con">
+            <h2 className="contact-info">+251 932 576 834</h2>
+          </div>
+          <div className="contact-info-con">
+            <h2 className="contact-info">info@mydomain.com</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
