537189d5eaf11a3b350dcd12112a1be25adebee6
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-03-18 09:48:19 +0300

    Working on footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,3 +1,17 @@
+import {
+  wp1,
+  wp2,
+  wp3,
+  wp4,
+  wp5,
+  wp6,
+  wp7,
+  wp8,
+  wp9,
+  wp10,
+  wp11,
+  wp12,
+} from "../../constants/images";
 import "./Footer.css";
 const Footer = () => {
   return (
@@ -13,6 +27,15 @@ const Footer = () => {
       </div>
       <div className="footer-latest">
         <h2 className="footer-title">Latest Blog</h2>
+        <div className="cards-container">
+          <img className="footer-img" src={wp1} />
+          <div className="desc-container">
+            <h2 className="footer-blog-title">
+              Even all the powerful pointing has no control about
+            </h2>
+            <h3 className="footer-blog-date">Oct16,2019</h3>
+          </div>
+        </div>
       </div>
       <div className="footer-information">
         <h2 className="footer-title">Information</h2>
