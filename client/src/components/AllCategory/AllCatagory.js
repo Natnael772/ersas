75528b893305244e4d23f20192d2742bd4ca756c
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-04-03 01:09:46 +0300

    Updated allcategories card UI

--- client/src/components/AllCategory/AllCatagory.js
+++ client/src/components/AllCategory/AllCatagory.js
@@ -81,10 +81,44 @@ const AllCategory = () => {
           </div>
         </div>
         <div className="cat-blog">
-          <img src={wp2} className="cat-blog-img" />
+          <img src={wp5} className="cat-blog-img" />
+          <p className="cat-name">
+            Business <span className="cat-blog-date"> - July 2, 2020</span>
+          </p>
+          <h2 className="cat-blog-title">
+            Your most unhappy customers are your greatest source of learning
+          </h2>
+          <p className="cat-blog-desc">
+            Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind here here ...
+          </p>
+          <div className="cat-user-con">
+            <img className="cat-user-img" />
+            <div className="cat-userinfo-con">
+              <h2 className="cat-userinfo-name">Sergy Campbell</h2>
+              <p className="cat-userinfo-bio">CEO and Founder</p>
+            </div>
+          </div>
         </div>
         <div className="cat-blog">
-          <img src={wp4} className="cat-blog-img" />
+          <img src={wp5} className="cat-blog-img" />
+          <p className="cat-name">
+            Business <span className="cat-blog-date"> - July 2, 2020</span>
+          </p>
+          <h2 className="cat-blog-title">
+            Your most unhappy customers are your greatest source of learning
+          </h2>
+          <p className="cat-blog-desc">
+            Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind here here ...
+          </p>
+          <div className="cat-user-con">
+            <img className="cat-user-img" />
+            <div className="cat-userinfo-con">
+              <h2 className="cat-userinfo-name">Sergy Campbell</h2>
+              <p className="cat-userinfo-bio">CEO and Founder</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
