886490c101581660df08c12f0d150a779d34d976
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-04-20 13:26:12 +0300

    modified Clap model

---
+++ server/models/comment.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema();
+const commentSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    red: "Post",
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model("Comment", commentSchema);
