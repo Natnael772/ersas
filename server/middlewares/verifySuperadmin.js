1f60177522eb93a6548c9390e9feb13b257a2f3a
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-06-02 18:20:18 +0300

    Verify superadmin

---
+++ server/middlewares/verifySuperadmin.js
@@ -0,0 +1,42 @@
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
+
+const Superadmin = require("../models/superadmin");
+module.exports = async (req, res, next) => {
+  //1. Getting token and check if it's there
+  console.log(req.headers);
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+    console.log(token);
+  }
+
+  if (!token) {
+    console.log("no token");
+    return res.status(401).json({
+      status: "fail",
+      msg: "You arenot authorized ",
+    });
+  }
+  //2. Verify token
+  // console.log(process.env.JWT_SECRET);
+  const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+  console.log(decoded);
+
+  // //3 Check if user still exists
+
+  const user = await Superadmin.findOne({ id: decoded.id });
+  if (!user) {
+    return res.status(404).json({
+      status: "fail",
+      msg: "The superadmin belonging to this token no longer exist",
+    });
+  }
+
+  //grant access to protected route
+  req.user = user;
+  next();
+};
