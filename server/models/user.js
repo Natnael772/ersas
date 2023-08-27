ed45af7ce027d029404297a0d440a52208c56781
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-08-27 12:52:47 +0300

    Modified user model

--- server/models/user.js
+++ server/models/user.js
@@ -31,19 +31,23 @@ const userSchema = new Schema({
   // },
 
   profilePhoto: String,
+  role: {
+    type: String,
+    enum: ["user", "admin", "superadmin"],
+  },
 
-  followers: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-  following: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
+  // followers: [
+  //   {
+  //     type: Schema.Types.ObjectId,
+  //     ref: "User",
+  //   },
+  // ],
+  // following: [
+  //   {
+  //     type: Schema.Types.ObjectId,
+  //     ref: "User",
+  //   },
+  // ],
 });
 
 module.exports = mongoose.model("User", userSchema);
