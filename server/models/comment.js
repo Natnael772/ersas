4d63f6d36df8441dd31ca3e240f20175c427bde1
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-08-27 12:55:16 +0300

    Modifed comment model

--- server/models/comment.js
+++ server/models/comment.js
@@ -2,30 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const commentSchema = new Schema(
   {
-    // content: {
-    //   type: String,
-    //   required: true,
-    // },
-
-    blog: {
+    authorId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+    blogId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Blog",
-      required: true,
     },
 
-    comments: [
-      {
-        user: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "User",
-          required: true,
-        },
-        content: {
-          type: String,
-          required: true,
-        },
-      },
-    ],
+    content: {
+      type: String,
+      required: true,
+    },
 
     // user: {
     //   type: mongoose.Schema.Types.ObjectId,
